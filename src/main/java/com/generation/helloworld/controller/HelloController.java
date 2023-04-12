1d89794cc0370576ffb945cb171ae0d2f543c4a9
Jeniffer Souza <jesouza774@gmail.com>
2023-04-12 19:25:35 -0300

    Ex. spring

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -12,9 +12,23 @@ public class HelloController {
 	public String hello() {
 		return "<b>Hello Turma 62 !</b>";
 	}
-	
+
 	@GetMapping("/nome")
 	public String meuNome() {
 		return "<b> Hello!<br />Jeniffer Souza </b>";
+	}
+
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "<b>Responsabilidade Pessoal<br />" + "Mentalidade de Crescimento<br />" + "Orientação ao Futuro<br />"
+				+ "Persistência<br />" + "Comunicação<br />" + "Orientação aos Detalhes<br />"
+				+ "Trabalho em Equipe<br />" + "Proatividade</b>";
+	}
+
+	@GetMapping("/objetivos")
+	public String objetivos() {
+		return "<b>Objetivos- Mentalidade de Crescimento<br /><br />"
+				+ "<b>Mentalidade de crescimento é a crença de que você pode melhorar ao longo dotempo com a prática.<br />"
+				 + "- Aprender o maximo possivel para dominar os assuntos. ";
 	}
 }
